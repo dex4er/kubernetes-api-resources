2994f0507696ef3d747a996c5dec70b87a8d4881
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-10-07 00:22:35 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export DOCKER_CLI_HINTS=false
+
+for v in $(jq -r '.[][1]|select((.|split(".")[1]|tonumber) >= 27)' versions.json); do
+  echo "## $v"
+  docker pull --disable-content-trust kindest/node:v$v
+  kind create cluster --config <(sed "s,image: kindest/node,&:v$v," config.yaml)
+  kubectl version
+  kubectl api-resources | awk '{if ($2 ~ /\//) print $1,"-",$2,$3,$4; else print $1,$2,$3,$4,$5}' | sort > api-resources-$v.txt
+  if [[ -n $previous ]]; then 
+    diff -u api-resources-$previous.txt api-resources-$v.txt
+  fi
+  kind delete cluster
+  previous=$v
+done
